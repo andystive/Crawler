ac3845aefab01e962720f04136466ca1e324d8cd
andystive <vvirus@foxmail.com>
2021-07-21 21:45:08 +0800

    2020-05-06 by virus

--- test.py
+++ test.py
@@ -1,5 +1,24 @@
-import requests
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-api = 'http://ip.jiangxianli.com/'
-response = requests.get(api)
-print(response.text)
+"""
+读取本地xml文件内容，并返回所需字段
+@Time    : 2020/07/20
+@Author  : Virus
+@FileName: test.py
+@Software: PyCharm
+"""
+
+import re
+
+#path = 'C:\Users\jobs\Desktop\a.xml'
+
+with open(r'C:\Users\jobs\Desktop\a.xml', 'r') as f:
+    content = f.readall()
+    #print(html)
+
+    patten = re.compile('<BioSample.*?<Attributes>(.*?)</Attributes>.*?</BioSample>')
+    items = re.findall(patten, content)
+    for item in items:
+        print(item)
+
